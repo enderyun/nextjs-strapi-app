d48eb4efad4e226c8decae8fd383ab0478fda982
enderyun <enderyum1524@gmail.com>
2026-03-07 01:22:54 -0500

    initial signin and signup forms

---
+++ frontend/app/(auth)/signin/page.tsx
@@ -0,0 +1,9 @@
+import { SigninForm } from "@/components/sing-in-form";
+
+export default function SignInRoute() {
+  return (
+    <div>
+      <SigninForm />
+    </div>
+  )
+}
---
+++ frontend/app/(auth)/signup/page.tsx
@@ -0,0 +1,9 @@
+import { SignupForm } from "@/components/sing-up-form";
+
+export default function SignUpRoute() {
+  return (
+    <div>
+      <SignupForm />
+    </div>
+  )
+}
